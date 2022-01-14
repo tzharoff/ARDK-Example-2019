8187cba905dffc611ff93569a8df8f10708d791d
Tony Zharoff <tony.zharoff@gmail.com>
2022-01-14 06:18:47 -0800

    Git-LFS

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Niantic.ARDKExamples.Helpers;
+
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField] private AudioSource SoundA;
+
+    private void OnEnable()
+    {
+        ARHitTester.LetterSpawn += PlaySoundA;
+    }
+
+    private void OnDisable()
+    {
+        ARHitTester.LetterSpawn -= PlaySoundA;
+    }
+
+    private void PlaySoundA()
+    {
+        SoundA.Play();
+    }
+
+}
